045545b2b12c14ba5e755b618d50307c139c7a84
Kathy <kchen2013@gmail.com>
2016-12-05 10:05:29 -0500

    adding docopt for the eADAGE version of this script

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,55 @@
-"""Top-level script"""
+"""
+This script iterates through a directory of ADAGE model weight matrices and
+determines the pathways significant in each model after crosstalk is removed.
 
+Output:
+    .txt tab-delimited files specifying the significant pathways
+    found in the model.
+    Columns:
+      model size, node #, side ('pos' or 'neg'), pathway, p-value, q-value
+
+Usage:
+	pipeline.py <models-dir> <output-dir>
+		<pathway-definitions> <gene-compendium>
+		[--cores=<n-cores>] [--replace=<replace>] [--std=<std-signature>]
+		[-a | --all-genes]
+    pipeline.py -h | --help
+
+Options:
+    -h --help                   Show this screen.
+
+    <models-dir>                Path to the directory containing ADAGE models
+    <output-dir>                Path to the directory that will store the
+                                output files. Will be created if no such path
+                                exists.
+    <pathway-definitions>       Path to the pathway definitions file.
+                                Formatted as tab-delimited columns:
+                                pathway, N (num. genes), geneA;geneB;...geneN
+    <gene-compendium>           Path to the gene compendium file.
+
+    --replace=<replace>         The resulting file keeps the naming convention
+                                of the input file, with the exception of this
+                                substring, which will be replaced by 'SigPathways'
+                                [default: network]
+    --cores=<n-cores>           Number of cores used to run crosstalk removal on
+                                models in parallel
+                                [default: num. available cores - 1]
+    --std=<std-signature>       Number of standard deviations from the mean
+                                gene expression value that defines the +/-
+                                cutoff for an ADAGE node's high positive weight
+                                and high negative weight gene signatures.
+                                [default: 2.5]
+
+    -a --all-genes              Apply crosstalk removal to the full gene set
+                                in each ADAGE node. By default, the procedure
+                                is only applied to genes in the node's gene
+                                signatures (genes with a high positive weight
+                                or high negative weight).
+
+
+"""
+
+from docopt import docopt
 import multiprocessing
 import os
 import pdb
@@ -12,14 +62,6 @@ import pandas as pd
 
 import mie
 import utils
-
-### initialize our constants ###  # TODO: replace with argparse
-STD_ABOVE = 2.5  # threshold used when identifying our gene signatures
-weight_dir = "data/EM300-10DIFF"  # ADAGE weight matrices
-significant_pathways_dir = "data/SIGPWS-10DIFF-PIPELINE"  # output directory
-gene_compendium = "data/all-pseudomonas-gene-normalized.pcl"  # gene compendium, only used for gene names
-pathway_definitions_file = "data/pathway_definitions.txt"  # pathways are defined as a group of genes
-substring_to_replace = "_ClusterByweighted_avgweight_"  # when writing to a file
 
 # read in the gene IDs from data file
 pcl_data = pd.read_csv(gene_compendium, sep="\t", usecols=["Gene_symbol"])
@@ -48,7 +90,6 @@ def process_model(weight_file):
 		significant_pathways_df = pd.concat([significant_pathways_df, node_df], axis=0)
 	significant_pathways_df.reset_index(drop=True, inplace=True)
 	
-	print(significant_pathways_df.shape)
 	output_filename = weight_file.replace(substring_to_replace, "_SigPathway_")
 	output_path = os.path.join(significant_pathways_dir, output_filename)
 	significant_pathways_df.to_csv(
@@ -56,11 +97,38 @@ def process_model(weight_file):
 	return True
 
 if __name__ == "__main__":
+	arguments = docopt(
+		__doc__, version="eADAGE pathway coverage without crosstalk, 0.0")
+	models_directory = arguments["<models-dir>"]
+	output_directory = arguments["<output-dir>"]
+	pathway_definitions_file = arguments["<pathway-definitions>"]
+	gene_compendium = arguments["<gene-compendium>"]
+	stddev = float(arguments["--std"])
+	substring_to_replace = arguments["--replace"]
+	all_genes = arguments["--all-genes"]
+
+	# create the output directory if it does not exist
+	try: 
+	    os.makedirs(arguments["<output-dir>"])
+	except OSError:
+	    if not os.path.isdir(arguments["<output-dir>"]):
+	        raise
+
+	if arguments["--cores"].isdigit():
+		n_cores = int(arguments["--cores"])
+	else:
+		n_cores = multiprocessing.cpu_count() - 1
+
+	# only need the gene IDs from the compendium file.
+	pcl_data = pd.read_csv(gene_compendium, sep="\t", usecols=["Gene_symbol"])
+	gene_ids = pcl_data.iloc[:,0]  # format: list(str) of gene IDs
+	
 	t_o = time()
 	np.seterr(all="raise")
-	n_cores = multiprocessing.cpu_count()
-	with Parallel(n_jobs=n_cores-1) as parallel:
-		results = parallel(delayed(process_model)(weight_file) for weight_file in os.listdir(weight_dir))
+	with Parallel(n_jobs=n_cores) as parallel:
+		results = parallel(
+			delayed(process_model)(weight_file)
+			for weight_file in os.listdir(models_directory))
 	t_f = time() - t_o
-	print("{0} models took {1} seconds to run on {2} cores.".format(len(results), t_f, n_cores-1))
-
+	print("{0} models took {1} seconds to run on {2} cores.".format(
+		len(results), t_f, n_cores))
